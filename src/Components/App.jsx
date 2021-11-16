9d80a4c6979295c7159917680db6aaa406be209d
ElizabethThompson1 <usawsoccer11@yahoo.com>
2021-11-16 13:17:13 -0600

    added images and wrote a nav bar

---
+++ src/Components/App.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+import Table from "./Table/Table";
+import SearchBar from "./SearchBar/SearchBar";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state={};
+    }
+
+    render(){
+        return(
+
+        )
+    }
+}
+
+export default App;
